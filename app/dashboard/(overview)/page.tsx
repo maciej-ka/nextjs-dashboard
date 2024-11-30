71b2616a776e21bd158fc051b30b2536519adaf9
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-11-30 18:18:09 +0100

    feat: separate streaming of latests invoices component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,14 +2,14 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData, fetchLatestInvoices } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 // import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
 
 export default async function Page() {
   // const revenue = await fetchRevenue()
-  const latestInvoices = await fetchLatestInvoices()
+  // const latestInvoices = await fetchLatestInvoices()
   const {
     numberOfCustomers,
     numberOfInvoices,
@@ -51,7 +51,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
