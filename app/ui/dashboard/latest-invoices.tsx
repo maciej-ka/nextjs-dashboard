75f5b5a8c9dbba851d12a9adf53bb724b66cabea
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-12-06 13:45:50 +0100

    fix: linter errors

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,9 +2,7 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
 import { fetchLatestInvoices } from '@/app/lib/data';
-// export default async function LatestInvoices({ latestInvoices }: { latestInvoices: LatestInvoice[] }) {
 export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices();
   return (
@@ -13,8 +11,6 @@ export default async function LatestInvoices() {
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: Uncomment this code in Chapter 7 */}
-
         <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
